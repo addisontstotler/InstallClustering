6231903bc6143a20b0881e15f1ae1cc633f57b4f
Addison Stotler <addisontstot@gmail.com>
2021-01-07 15:35:30 -0600

    Conversion from Visual output to console output
    Changed the output from a picture, to a printed list of locations.
    
    Added another line to the input csv file. The len()-1's on lines 16, 91, and 96.

--- installGroupsClustering.py
+++ installGroupsClustering.py
@@ -8,35 +8,38 @@ import _tkinter
 
 #Number of clusters
 numK = 8
-iterations = 7
+iterations = 20
 
 #euclidD: Calculate the euclidian distance between point1 and point2
 def euclidD(point1,point2):
     sum = 0
-    for index in range(len(point1)):
+    for index in range(len(point1)-1):
         diff = (point1[index]-point2[index]) ** 2
         sum = sum + diff
 
     euclidDistance = math.sqrt(sum)
     return euclidDistance
     
-#readFile: Reads the file and returns datadict (hash table of lat and lon)
+#readFile: Reads the file and returns datadict (hash table of lat, lon, and branchName)
+    #Lat and Lon are used for clustering and branchName is only used for the output file.
 def readFile(filename):
     #Opens the file
     datafile = open(filename,"r")
-
     #Creates new data dictionary (hash table)
     datadict = {}
     key = 0
 
     #Loops through file and adds each line to datadic
     for line in datafile:
-        items = line.split()
+        line.replace("\n",'')
+        items = line.split(',')
         key = key + 1
         lat = float(items[0])
         lon = float(items[1])
-        datadict[key] = [lon,lat]
-        
+        branchName = items[2]
+        datadict[key] = [lon,lat,branchName]
+
+    datafile.close()
 
     return datadict
 
@@ -60,7 +63,6 @@ def createCentroides(k, datadict):
 
 #createClusters:
 def createClusters(k, centroids, datadict, repeats):
-    #What it does
     for apass in range(repeats):
         print("****PASS",apass,"****")
 
@@ -69,29 +71,29 @@ def createClusters(k, centroids, datadict, repeats):
         for i in range(k):
             clusters.append([])
 
-        #What it does
         for key in datadict:
+            #Creates a temporary list of distances
             distances= []
 
-            #What it does
+            #Fills the list with distances
             for clusterIndex in range(k):
                     #Calculates the distance between a point and the centroid
                     dist = euclidD(datadict[key],centroids[clusterIndex])
                     #Add distance to distances list
                     distances.append(dist)
 
+            #Finds the smallest distance between branches and adds it to the cluster.
             mindist = min(distances)
             index = distances.index(mindist)
-            
             clusters[index].append(key)
 
         #What now?
-        dimensions = len(datadict[1])
+        dimensions = len(datadict[1])-1
         for clusterIndex in range(k):
             sums = [0]*dimensions
             for akey in clusters[clusterIndex]:
                 datapoints = datadict[akey]
-                for ind in range(len(datapoints)):
+                for ind in range(len(datapoints)-1):
                     sums[ind] = sums[ind] + datapoints[ind]
             for ind in range(len(sums)):
                 clusterLen = len(clusters[clusterIndex])
@@ -101,19 +103,20 @@ def createClusters(k, centroids, datadict, repeats):
 
             centroids[clusterIndex] = sums
 
-##      for c in clusters:
-##          print("CLUSTER")
-##          for key in c:
-##              print(datadict[key], end=" ")
-##          print()
+#Test output
+#-------------------------------------------
+#      for c in clusters:
+#          print("CLUSTER")
+#          for key in c:
+#              print(datadict[key], end=" ")
+#          print()
+#-------------------------------------------
     return clusters
 
-##visualize: Plots clusters on the map
 
-#Outputs an image of the map of the united states
-#Dots are placed on branch locations. Their color corrolates
-#with the region/install grourp cluster.
-def visualizeBranches(dataFile):
+#clusterBranches: Lists the clusters of branches
+
+def clusterBranches(dataFile):
     #Calls functions to create lists and data dictionaries
     datadict = readFile(dataFile)
 
@@ -121,52 +124,37 @@ def visualizeBranches(dataFile):
     
     clusters = createClusters(numK, branchCentroids, datadict, iterations)
 
-    #Turtle objects
-    branchT = turtle.Turtle()
-    branchWin = turtle.Screen()
-    branchWin.colormode(255)
-    branchWin.bgpic("worldmap3x.gif")
-
-    #Used to convert lat lon into width and height on the image
-    wFactor = (branchWin.screensize()[0]/2)/180
-    hFactor = (branchWin.screensize()[1]/2)/90
-
-    #List of different colors for the clusters
-    colorlist = [(165,0,38),(215,48,39),(244,109,67),
-                 (253,174,97),(254,224,144),(224,243,248),
-                 (171,217,233),(116,173,209),(69,117,180),
-                 (49,54,149),(234,124,159),(255,0,0),
-                 (0,255,0),(0,0,255),(255,255,0),
-                 (0,255,255)]
-
     sortList = {}
 
     #What it do: Sorts stuff... but why?
+    #numK is the number of clusters
     for clusterIndex in range(numK):
         sortList[len(clusters[clusterIndex])] = clusterIndex
+        
     sortKeys = list(sortList.keys())
     sortKeys.sort()
     sortKeys.reverse()
     print(sortList)
 
 
+    #Outputs the results of the clustering
     for sKey in sortKeys:
         clusterIndex = sortList[sKey]
-        branchT.color(colorlist[clusterIndex])
         for aKey in clusters[clusterIndex]:
             lon = datadict[aKey][0]
             lat = datadict[aKey][1]
-            branchT.goto(lon*wFactor,lat*hFactor)
-            branchT.dot()
-    branchWin.exitonclick()
+            print(datadict[aKey][2].replace('\n',''))
+        print()
+
+    return
 
 #visualizeQuake("cluster.txt")
-
 def main():
-    filename = input('What is the name of the file that contains the lat lon data?(branches.csv)')
-    visualizeBranches(filename)
+    #Filename of a csv formated as: "lat,lon,branchName"
+    latLonFileName = input('What is the name of the file that contains the lat lon data?(zipcodes_locations.csv)')
+    clusterBranches(latLonFileName)
     
-
+    
     return
 
 main()
